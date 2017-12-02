67b393500107f0838263d7ce19e65505969b2b4f
Juan Jesús Torre Tresols <juanjesustorre@gmail.com>
2017-12-02 21:19:48 +0100

    Update probability.m

--- probability.m
+++ probability.m
@@ -39,6 +39,7 @@ if x == 'y'                           %%If the answer is 'y', it continues
     end
 elseif x == 'n'                       %%If the answer is 'n', it asks for
     fprintf('\n')                     %%the new value of p
+                                      %%If the answer is 'n', it asks for
     prompt2 = '    ...oh. Well, then what''s your coin''s probability of going heads? (Input an integer between 1 and 100): ';
     y = input(prompt2);
     fprintf('\n')
