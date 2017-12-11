45c61ec44e9330b1c90a5f08855a8a06d8968ac0
Juan Jesús Torre Tresols <juanjesustorre@gmail.com>
2017-12-11 18:20:27 +0100

    Update probability.m
    It's the same, but now it has 13 lines less of code

--- probability.m
+++ probability.m
@@ -22,21 +22,8 @@ fprintf('    The probability of...wait.\n') %%The program asks if p ~= 0.5
 prompt1 = '    Sorry for asking this, but I hope that your coin''s probability of going heads is 50%, right? (y/n): ';
 x = lower(input(prompt1, 's'));       
                                       
-if x == 'y'                           %%If the answer is 'y', it continues
-    fprintf('\n')                     %%normally with the p's default value
-    fprintf('    Thank you, sorry for asking such a silly question. Then:\n')
-    fprintf('\n')
-    if nargin == 1                    %%If only n was provided, then it
-        for k = 0:n                   %%calculates for al possible k values
-            A = nchoosek(n,k);
-            prob = A*(p^k)*((1-p)^(n-k));
-            fprintf('    The probability of getting %d heads in %d flips is %.6f%%\n', k, n, prob)
-        end
-    else                              %%If k is provided too, it only
-        A = nchoosek(n,k);            %%calculates for that value of k
-        prob = A*(p^k)*((1-p)^(n-k));
-        fprintf('    The probability of getting %d heads in %d flips is %.6f%%\n', k, n, prob)
-    end
+if x == 'y'                           %%If the answer is 'y', it continues normally with the p's default value
+    fprintf('\n    Thank you, sorry for asking such a silly question. Then:\n\n')
 elseif x == 'n'                       %%If the answer is 'n', it asks for
     fprintf('\n')                     %%the new value of p
     prompt2 = '    ...oh. Well, then what''s your coin''s probability of going heads? (Input an integer between 1 and 100): ';
@@ -44,25 +31,25 @@ elseif x == 'n'                       %%If the answer is 'n', it asks for
     fprintf('\n')
     if y < 1 || y > 100 || floor(y) ~= y || ~isscalar(y)  %%Check if input is an integer between 1 and 100
        fprintf('    That is not a valid answer. Please try again!\n')
+       return
     else
         p = y/100;                    %%New value of p is set
-        fprintf('    Such a rare coin you have there. None of my business, however. Then:\n')
-        fprintf('\n')
-        if nargin == 1                %%Same code as in the 'y' answer
-            for k = 0:n               %%block for n and n,k provided
-                A = nchoosek(n,k);
-                prob = A*(p^k)*((1-p)^(n-k));
-                fprintf('    The probability of getting %d heads in %d flips is %.6f%%\n', k, n, prob)
-            end
-        else
-            A = nchoosek(n,k);
-            prob = A*(p^k)*((1-p)^(n-k));
-            fprintf('    The probability of getting %d heads in %d flips is %.6f%%\n', k, n, prob)
-        end
+        fprintf('    Such a rare coin you have there...none of my business, however. Then:\n\n')
     end        
-else                                 %%If the answer is not 'y', 'Y', 'n'
-    fprintf('\n')                    %%or 'N', this message is shown
-    fprintf('    That''s not a valid answer, try again!\n')
+else                                 %%If the answer is not 'y', 'Y', 'n' or 'N', this message is shown
+    fprintf('\n    That''s not a valid answer, try again!\n')
+    return
 end
 
+if nargin == 1                    %%If only n was provided, then it
+    for k = 0:n                   %%calculates for al possible k values
+        A = nchoosek(n,k);
+        prob = A*(p^k)*((1-p)^(n-k));
+        fprintf('    The probability of getting %d heads in %d flips is %.6f%%\n', k, n, prob)
+    end
+else                              %%If k is provided too, it only
+    A = nchoosek(n,k);            %%calculates for that value of k
+    prob = A*(p^k)*((1-p)^(n-k));
+    fprintf('    The probability of getting %d heads in %d flips is %.6f%%\n', k, n, prob)
+end
 fprintf('\n')
